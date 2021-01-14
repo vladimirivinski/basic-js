3e8e4a008eb38a09baee35c21f8e02892748714d
Vladimir <vladimir.ivinski89@gmail.com>
2021-01-14 19:41:18 +0300

    feat: add code to count-cats.js

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,15 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function countCats(matrix) {
+  throw new CustomError("Not implemented");
+
+  let catCounter = 0; // объявляем переменную для счетчика
+  matrix.map((cal1, val) => {
+    // проходим по данному в условии массиву и вызываем
+    cal1.map((cal2, val) => {
+      // map  в получунном callback с условием, если значение
+      if (cal2 === "^^") catCounter++; // равно, +1 к счетчику
+    });
+  });
+  return catCounter;
 };
