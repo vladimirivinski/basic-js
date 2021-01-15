00d6c761a6a409c6f0ca3a84378ec6c0c9f4c81b
Vladimir <vladimir.ivinski89@gmail.com>
2021-01-15 03:35:20 +0300

    feat: another solution

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,15 +1,12 @@
-const CustomError = require("../extensions/custom-error");
-
 module.exports = function countCats(matrix) {
-  throw new CustomError("Not implemented");
-
-  let catCounter = 0; // объявляем переменную для счетчика
-  matrix.map((cal1, val) => {
-    // проходим по данному в условии массиву и вызываем
-    cal1.map((cal2, val) => {
-      // map  в получунном callback с условием, если значение
-      if (cal2 === "^^") catCounter++; // равно, +1 к счетчику
-    });
-  });
-  return catCounter;
+  const res = matrix.flat().filter((word) => word == "^^").length;
+  return res;
 };
+
+// let catCounter;
+// matrix.map((cal1, a) => {
+//   cal1.map((cal2, b) => {
+//     if (cal2 === "^^") catCounter++;
+//   });
+// });
+// return catCounter;
