0e45f76adff6fc229271bcb2560397069acb3481
Vladimir <vladimir.ivinski89@gmail.com>
2021-01-15 19:21:19 +0300

    feat: add resolution

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,13 @@
-const CustomError = require("../extensions/custom-error");
-
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  const result = [];
+  if (Array.isArray(members)) {
+    members.forEach((element) => {
+      if (typeof element === "string") {
+        result.push(element.trim().charAt(0).toUpperCase());
+      }
+    });
+    return result.sort().join("");
+  } else {
+    return false;
+  }
 };
